58b16698047facf5a7eff7eedc81580cc4fc6e84
priyanshu <priyanshukumari93933@gmail.com>
2025-03-29 18:29:04 +0530

    nextjs auth v5

--- src/app/db/DbConnect.js
+++ src/app/db/DbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const DbConnect=async()=>{
     try {
-        await mongoose.connect("your connection string")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Db connected")
     } catch (error) {
         console.log(error)
